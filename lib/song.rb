bf1f97fd82d62e703326c5ec4f2292a4b922d419
damaris <mdamarisnduku@gmail.com>
2022-08-22 12:03:00 +0300

    updates

--- lib/song.rb
+++ lib/song.rb
@@ -4,16 +4,11 @@ class Song
   extend Memorable::ClassMethods
   extend Findable::ClassMethods
   include Paramable::InstanceMethods
+  include Memorable::InstanceMethods
   attr_accessor :name
   attr_reader :artist
 
   @@songs = []
-
-  def initialize
-    @@songs << self
-  end
-
-  
 
   def self.all
     @@songs
